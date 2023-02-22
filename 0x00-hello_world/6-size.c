c4cbba5e636d2163a3bfcf3db122f632067a944f
IZiadI <ziadashraf727@gmail.com>
2023-02-21 17:22:49 -0800

    1

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %lu byte(s)", sizeof(c));
+	printf("Size of an int: %lu byte(s)", sizeof(i));
+	printf("Size of a long int: %lu byte(s)", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)", sizeof(ll));
+	printf("Size of a float: %lu byte(s)", sizeof(f));
+	return (0);
+}
